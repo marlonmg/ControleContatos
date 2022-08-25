89e912d2424790d901c6ec088d58da0df9160c86
Marlon Grazzianne Martins Araujo <marlonmgm@hotmail.com>
2022-08-25 18:12:01 -0300

    Primeiro commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        [Route("Contatos")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+    }
+}
